be67d5175e1d8505d3b718f6b0e5ea2290268f98
Yanuar Lukman Arif <yanuarlukmanarif@gmail.com>
2025-04-28 18:28:07 +0700

    memperbaiki settingan tailwind

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,7 +8,7 @@ import {
 
 export default function Home() {
   return (
-    <div className="flex flex-col min-h-screen">
+    <div className="flex flex-col min-h-screen ">
       <HeroSection />
       <FeaturedItems />
       <CategoriesSection />
