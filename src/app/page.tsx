de83456b7f3cee0b29cbcc5c6efc8d3f5ff57786
Yanuar Lukman Arif <yanuarlukmanarif@gmail.com>
2025-05-09 17:31:57 +0700

    mencoba memperbaiki di localhost allprodukgrid berfungsi atau sync dengan database, tapi di vercel tidak
    bedanya ternyata di cara memanggil component'nya
    
    the different is in here products={products || []} and products={dbProducts || []}

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,14 +8,14 @@ import { getProducts } from "@/actions/product-actions";
 
 export default async function Home() {
   // Fetch products from the database
-  const { products } = await getProducts(8);
+  const { products: dbProducts } = await getProducts(8);
 
   return (
     <div className="flex flex-col min-h-screen ">
       <HeroSection />
       <div className="container mx-auto px-4 sm:px-6 lg:px-8 py-12 pb-0">
         <AllProductsGrid
-          products={products || []}
+          products={dbProducts || []}
           initialLimit={8}
           incrementAmount={4}
         />
